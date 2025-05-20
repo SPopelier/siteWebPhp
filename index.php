87316620d30bc244c290a4ec8fabb9ab9f79f439
Sarah Popelier <sarah.popelier@hotmail.fr>
2025-05-20 15:28:03 +0200

    itéartion 4 ok

--- index.php
+++ index.php
@@ -74,4 +74,3 @@ $page_description = "Page Accueil";
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
     <script src="script.js"></script>
 </body>
-</html> 
